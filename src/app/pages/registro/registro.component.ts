737a84f21d30bc1dd8e7ac63aad93025cb6ba09c
Vane737 <vivian_vanessa_flores@hotmail.com>
2023-04-18 15:28:26 -0400

    Formulario con enfoque en templates implementados,generando mensajes de error al validar

--- src/app/pages/registro/registro.component.ts
+++ src/app/pages/registro/registro.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { NgForm } from '@angular/forms';
+import { UsuarioModel } from '../../models/usuario.model';
+
 
 @Component({
   selector: 'app-registro',
@@ -7,10 +10,22 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RegistroComponent implements OnInit {
 
+  usuario: UsuarioModel; // Instancia de Usuario Model
 
   constructor() { }
 
-  ngOnInit() { }
+  ngOnInit() {
+    this.usuario = new UsuarioModel(); //Inicializacion
+   }
+   onSubmit( form: NgForm ) {
 
+    // if ( form.form.status == "INVALID") {
+    //   return ;
+    // }
+    console.log("Formulario Enviado");
+    console.log(this.usuario);
+    console.log( form);
+
+   }
 
 }
